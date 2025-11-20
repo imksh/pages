c3ce1ee557137d3c352e9edef91477130e4a28f8
Karan Sharma <karan03945@gmail.com>
2025-11-20 15:09:23 +0530

    fixed redirects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-pink-200">
+    <div className="min-h-screen w-screen flex flex-col items-center justify-center bg-pink-200">
       <h1 className="text-4xl font-bold mb-10">Welcome</h1>
 
       <div className="flex flex-col gap-5">
