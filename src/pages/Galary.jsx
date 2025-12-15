f686b87215b50fb1646d042cbde8e298b825bccf
Karan Sharma <karan03945@gmail.com>
2025-12-16 02:17:50 +0530

    responsiveness fixed

--- src/pages/Galary.jsx
+++ src/pages/Galary.jsx
@@ -5,17 +5,23 @@ const Galary = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen w-screen flex flex-col items-center justify-center bg-blue-200">
-      <h1 className="text-4xl font-bold mb-10">Welcome To Galary Page</h1>
+    <div>
+      <div className="bg-blue-600 text-white h-[10vh] w-full fixed top-0 left-0 flex items-center px-2 lg:px-10 justify-between">
+        <div className="flex gap-3 items-center">
+          <h1 className="text-2xl lg:text-3xl font-bold">Galary</h1>
+        </div>
+        <p className="hidden lg:block font-bold"></p>
+      </div>
 
-      <div className="flex flex-col gap-5">
-        
-        <button
-          onClick={() => navigate("/galary/karan-galary")}
-          className="px-6 py-3 bg-blue-500 text-white rounded-md cursor-pointer"
-        >
-          Karan
-        </button>
+      <div className="min-h-[90dvh] mt-[10vh] w-screen flex flex-col items-center justify-center bg-blue-200">
+        <div className="flex flex-col gap-5">
+          <button
+            onClick={() => navigate("/galary/karan-galary")}
+            className="px-6 py-3 bg-blue-500 text-white rounded-md cursor-pointer"
+          >
+            Karan
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
 
   return (
     <div>
-      <div className="bg-blue-600 text-white h-[10vh] w-full fixed top-0 left-0 flex items-center px-10 justify-between">
+      <div className="bg-blue-600 text-white h-[10vh] w-full fixed top-0 left-0 flex items-center px-2 lg:px-10 justify-between">
         <div className="flex gap-3 items-center">
-          <Lottie animationData={infinity} loop={true} className="w-16" />
-          <h1 className="text-3xl font-bold">Pages</h1>
+          <Lottie animationData={infinity} loop={true} className="w-12 lg:w-16" />
+          <h1 className="text-2xl lg:text-3xl font-bold">Pages</h1>
         </div>
-        <p className="font-bold">A Collection of Works</p>
+        <p className="hidden lg:block font-bold">A Collection of Works</p>
       </div>
       <div className="min-h-[90dvh] w-screen flex flex-col items-center justify-center bg-blue-200 mt-[10vh]">
         <div className="flex flex-col gap-5">
